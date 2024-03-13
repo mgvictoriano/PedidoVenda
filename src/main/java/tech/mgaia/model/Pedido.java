af51b789dd4987b983fb2d9db3ca5e1fda614efe
Michelle Victoriano <desenvolvimento12@sifat.com.br>
2024-03-13 16:41:13 -0300

    feat: criada entidade pedido

---
+++ src/main/java/tech/mgaia/model/Pedido.java
@@ -0,0 +1,65 @@
+package tech.mgaia.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "pedido")
+public class Pedido implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, length = 30, name = "data_criacao")
+    private Date dataCriacao;
+
+    @Temporal(TemporalType.DATE)
+    @Column(nullable = false, length = 10, name = "data_entrega")
+    private Date dataEntrega;
+
+    @Column(columnDefinition = "text")
+    private String observacao;
+
+    @Column(nullable = false, precision = 10, scale = 2, name = "valor_total")
+    private BigDecimal valorTotal;
+
+    @Column(nullable = false, precision = 10, scale = 2, name = "valor_desconto")
+    private BigDecimal valorDesconto;
+
+    @Column(name = "valor_frete", nullable = false, precision = 10, scale = 2)
+    private BigDecimal valorFrete;
+
+    @Column(nullable = false, length = 30, name = "status")
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+    @Column(nullable = false, length = 30, name = "forma_pagamento")
+    private FormaPagamento formaPagamento;
+
+    @ManyToOne
+    @JoinColumn(name = "vendedor_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_vendedor"))
+    private Usuario vendedor;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
+    private Cliente cliente;
+
+    @Embedded
+    private EnderecoEntrega enderecoEntrega;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ItemPedido> itens = new ArrayList<>();
+}
