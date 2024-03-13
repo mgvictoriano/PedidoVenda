5b6109f1bc6eda9aa496da7a09072bf9409686b7
Michelle Victoriano <desenvolvimento12@sifat.com.br>
2024-03-13 16:51:01 -0300

    feat: ajuste na entidade usuario

--- src/main/java/tech/mgaia/model/Usuario.java
+++ src/main/java/tech/mgaia/model/Usuario.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 @Table(name = "usuario")
 public class Usuario implements Serializable {
 
-    private static final long serialVersionUID = XX;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue
